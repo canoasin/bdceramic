6dc4497c5e7a3b2beed9737e450af1960413ecd2
canoasin <50690804+canoasin@users.noreply.github.com>
2021-08-11 22:08:57 +0900

    Add files via upload

--- pjt/src/App.js
+++ pjt/src/App.js
@@ -1,7 +1,9 @@
+import './App.css'
+
 function App() {
   return (
     <div className="App">
-      <p style={{ textAlign: 'center' }}>bdceramic 프로젝트를 시작해봅시다.</p>
+      <p>bdceramic 프로젝트를 시작해봅시다.</p>
     </div>
   )
 }
